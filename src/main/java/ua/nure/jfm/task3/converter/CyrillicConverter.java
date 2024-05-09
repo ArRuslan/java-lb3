b3f82f4cefd0bc8916222d2a65df7539ba35665f
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-05-09 19:09:12 +0300

    implement CyrillicConverter

--- src/main/java/ua/nure/jfm/task3/converter/CyrillicConverter.java
+++ src/main/java/ua/nure/jfm/task3/converter/CyrillicConverter.java
@@ -6,24 +6,59 @@ import static ua.nure.jfm.task3.Utils.parseInt;
 public class CyrillicConverter {
 
 	private static char[][] DIGITS = {
-			{ '\u0430', '\u0432', '\u0433', '\u0434', '\u0454', '\u0455', '\u0437', '\u0438', '\u0473'},
+			// а   0     в   1     г   2     д   3     є   4     ѕ   5     з   6     и   7     ѳ   9
+			{'\u0430', '\u0432', '\u0433', '\u0434', '\u0454', '\u0455', '\u0437', '\u0438', '\u0473'},
+			// і  10     к  20     л  30     м  40     н  50     ѯ  60     о  70     п  80     ч  90
 			{'\u0456', '\u043A', '\u043B', '\u043C', '\u043D', '\u046F', '\u043E', '\u043F', '\u0447'},
+			// р 100     с 200     т 300     у 400     ф 500     х 600     ѱ 700     ѡ 800     ц 900
 			{'\u0440', '\u0441', '\u0442', '\u0443', '\u0444', '\u0445', '\u0471', '\u0461', '\u0446'}
 		};
 	
 	/**
 	 * The thousands sign to multiply the number's value.
 	 */
-	private static char kMul = '\u0482';
+	private static char kMul = '\u0482'; // ҂
 	
 	public static String convert(String input) {
 		int number = parseInt(input);
 
 		StringBuilder result = new StringBuilder();
 
-		// TODO: implement
+		int thousands = number / 1000;
+
+		if (thousands > 0) {
+			appendDigits(result, thousands, true);
+			number -= thousands * 1000;
+		}
+
+		appendDigits(result, number, false);
 
 		return result.toString();
+	}
+
+	private static void appendDigits(StringBuilder result, int number, boolean isThousands) {
+		int digits = number % 10;
+		int tens = (number / 10) % 10;
+		int hundreds = (number / 100) % 10;
+
+		if(hundreds > 0) {
+			if(isThousands) {
+				result.append(kMul);
+			}
+			result.append(DIGITS[2][hundreds-1]);
+		}
+		if(tens > 0) {
+			if(isThousands) {
+				result.append(kMul);
+			}
+			result.append(DIGITS[1][tens-1]);
+		}
+		if(digits > 0) {
+			if(isThousands) {
+				result.append(kMul);
+			}
+			result.append(DIGITS[0][digits-1]);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -40,5 +75,4 @@ public class CyrillicConverter {
 		System.out.printf("Maximum value (%s):%n", s);
 		System.out.println(CyrillicConverter.convert(s));
 	}
-
 }
